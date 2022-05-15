1b7aac7e1d66c1a357433be3458965591ec70f53
kdivriotis <konstantinos.divriotis@gmail.com>
2022-05-15 17:17:53 +0300

    Add route for executive funding per company

--- src/routes/executives.js
+++ src/routes/executives.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const { getExecutives } = require("../controllers/executives/get-executives");
+const {
+  getFundingPerCompany,
+} = require("../controllers/executives/get-funding-per-company");
 
 const router = express.Router();
 
 router.get("/", getExecutives);
+router.get("/funding-per-company", getFundingPerCompany);
 
 module.exports = router;
