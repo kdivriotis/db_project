150523b00400f7c01fff8c6d18715c524ae69dbc
kdivriotis <konstantinos.divriotis@gmail.com>
2022-05-22 23:39:24 +0300

    Add port in database configuration (+connection)

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 module.exports.dbHost = "127.0.0.1";
+module.exports.dbPort = 3306;
 module.exports.dbUser = "root";
 module.exports.dbPassword = "12345678kD!";
 module.exports.dbDatabase = "db_project";
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,8 +1,15 @@
 const mysql = require("mysql2");
-const { dbDatabase, dbHost, dbUser, dbPassword } = require("../config/db");
+const {
+  dbDatabase,
+  dbHost,
+  dbPort,
+  dbUser,
+  dbPassword,
+} = require("../config/db");
 
 const poolNoPromise = mysql.createPool({
   host: dbHost,
+  port: dbPort,
   user: dbUser,
   password: dbPassword,
   database: dbDatabase,
